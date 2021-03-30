62a04c9340abb90007259446988ba56b809ee46a
ykrop142 <63842913+ykrop142@users.noreply.github.com>
2021-03-30 15:08:53 +0700

    Add files via upload

---
+++ php func/nick.php
@@ -0,0 +1,15 @@
+<?php
+    $text='@te123st1ert rtytry^%$%&*$@';
+    function highlight_nicknames($text){
+        $pat='/\s([@^][^0-9\W]+([^\W][a-zA-Z\d])*)\s/';
+        $nik=preg_match ($pat,$text,$nickname);
+        if (preg_match ($pat,$text)){
+        $repl= '<b>'.$nickname[0].'</b>';
+        $res=preg_replace($pat,$repl,$text);
+        return $res;
+        }else{
+            return $text;
+        }
+    }
+    echo (highlight_nicknames($text));
+?>
