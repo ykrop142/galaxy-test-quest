ce0b5bb803bb1228472648588cffe933eaabe53a
ykrop142 <63842913+ykrop142@users.noreply.github.com>
2021-04-09 09:18:54 +0700

    Update nick.php

--- php func/nick.php
+++ php func/nick.php
@@ -4,9 +4,9 @@
         $pat='/\s([@^][^0-9\W]+([^\W][a-zA-Z\d])*)\s/';
         $nik=preg_match ($pat,$text,$nickname);
         if (preg_match ($pat,$text)){
-        $repl= '<b>'.$nickname[0].'</b>';
-        $res=preg_replace($pat,$repl,$text);
-        return $res;
+            $repl= '<b>'.$nickname[0].'</b>';
+            $res=preg_replace($pat,$repl,$text);
+            return $res;
         }else{
             return $text;
         }
